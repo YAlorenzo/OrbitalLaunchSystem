f48b284857f7090621128cfd8f037ea4b68e98d5
YAlorenzo <zhenia.klimenchuk@gmail.com>
2025-12-26 15:44:59 +0100

    clean

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import controller.LaunchDirector;
 import view.MissionControlConsole;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         MissionControlConsole console = new MissionControlConsole();
